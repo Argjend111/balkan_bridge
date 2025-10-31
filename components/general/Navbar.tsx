6d70794fbc28d773e4b87fd8919818dc2e51acba
Your Name <your-email@example.com>
2025-10-31 18:44:48 +0100

    changes

--- components/general/Navbar.tsx
+++ components/general/Navbar.tsx
@@ -1,12 +1,20 @@
 import Link from "next/link";
+import Logo from "@/public/logo.png"
+import Image from "next/image";
+import { Button } from "../ui/button";
+
 
 export default function Navbar(){
     return (
         <nav className="flex items-center justify-between py-5">
-            <Link href="/">
-            <h1 className="text-2xl font-bold">Job
-                <span className="text-primary">Argjend</span></h1>
+            <Link href="/" className="flex items-center gap-2">
+            <Image src={Logo} alt="logo" width={40} height={40}/>
+            <h1 className="text-2xl font-bold">Balkan
+                <span className="text-primary">Bridge</span></h1>
             </Link>
+            <Button className="cursor-pointer">
+                Login
+            </Button>
         </nav>
     )
 }
