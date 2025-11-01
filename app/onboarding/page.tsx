d3f6cecd1a82de09a360205d27c9b81f3a25e7a9
Your Name <your-email@example.com>
2025-11-01 23:27:47 +0100

    changes

--- app/onboarding/page.tsx
+++ app/onboarding/page.tsx
@@ -1,7 +1,9 @@
+import OnboardingForm from "@/components/forms/onboarding/OnboardingForm";
+
 export default function OnboardingPage(){
     return(
-        <div>
-            Hello
+        <div className="min-h-screen w-screen flex flex-col items-center justify-center py-10">
+            <OnboardingForm/>
         </div>
     )
 }
---
+++ components/forms/onboarding/CompanyForm.tsx
@@ -0,0 +1,7 @@
+export function CompanyForm(){
+    return(
+        <div>
+            <h1>Hello Company</h1>
+        </div>
+    )
+}
