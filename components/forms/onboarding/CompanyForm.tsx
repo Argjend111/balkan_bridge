e6a7cc0e6ed9711e2c3bb5ad32f66cff1b3241a8
Your Name <your-email@example.com>
2025-11-02 11:49:43 +0100

    changes

--- components/forms/onboarding/CompanyForm.tsx
+++ components/forms/onboarding/CompanyForm.tsx
@@ -1,7 +1,116 @@
-export function CompanyForm(){
-    return(
-        <div>
-            <h1>Hello Company</h1>
-        </div>
-    )
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { companySchema } from "@/app/utils/zodSchema";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+
+import z from "zod"
+import { Input } from "@/components/ui/input";
+import { Select, SelectContent, SelectGroup, SelectItem, SelectLabel, SelectTrigger, SelectValue } from "@/components/ui/select";
+import { countryList } from "@/app/utils/countriesList";
+
+export function CompanyForm() {
+    const form = useForm<z.infer<typeof companySchema>>({
+        resolver: zodResolver(companySchema),
+        defaultValues: {
+            about: "",
+            location: "",
+            logo: "",
+            website: "",
+            xAccount: "",
+        },
+    });
+
+    return (
+        <Form {...form}>
+            <form className="space-y-6">
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Company Name</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Enter company name" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="location"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Company Location</FormLabel>
+                                <Select onValueChange={field.onChange}
+                                    defaultValue={field.value}
+                                >
+                                    <FormControl>
+                                        <SelectTrigger>
+                                            <SelectValue placeholder="Select Location" />
+                                        </SelectTrigger>
+                                    </FormControl>
+                                    <SelectContent>
+                                        <SelectGroup>
+                                            <SelectLabel>
+                                                WorldWide
+                                            </SelectLabel>
+                                            <SelectItem value="worldwide">
+                                                <span></span><span>Worldwide / Remote</span>
+                                            </SelectItem>
+                                        </SelectGroup>
+                                            <SelectGroup>
+                                            <SelectLabel>
+                                                Location
+                                            </SelectLabel>
+                                            {countryList.map((country) => (
+                                            <SelectItem value={country.name} key={country.code}>
+                                                <span>{country.flagEmoji}</span>
+                                                <span className="pl-2">{country.name}</span>
+                                                
+                                               
+                                            </SelectItem>
+                                             ))}
+                                        </SelectGroup>
+                                    </SelectContent>
+                                </Select>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                      <FormField
+                        control={form.control}
+                        name="website"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Website</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="https://yourCompany.com" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField
+                        control={form.control}
+                        name="xAccount"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>X (Twitter) Account</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="@youcompany" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                </div>
+
+                
+            </form>
+        </Form>
+    );
 }
